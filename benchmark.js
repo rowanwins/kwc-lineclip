543eb1688d42e7152cf54808b3f34c5538760d15
rowanwins <rowanwins@yahoo.com.au>
2018-02-02 16:15:36 +1100

    Initial commit multi-vertice lines, still to fix horizontal & vertical

--- benchmark.js
+++ benchmark.js
@@ -5,14 +5,15 @@ var lineclip = require('lineclip');
 
 const bbox = [-5, -5, 5, 5];
 
-new Suite()
+console.log("SINGLE SEGMENT TEST")
+new Suite('Single Segment')
   .add('kwc', () => {
     var a = [-10, -10], b = [10, 10]
     kwc([a, b], bbox);
   })
-  .add('liang-barsky', () => {
+  .add('liang-barsky (non-destructive)', () => {
     var a = [-10, -10], b = [10, 10]
-    lb(a, b, bbox);
+    lb(a, b, bbox, [], [[],[]]);
   })
   .add('mapbox/lineclip', () => {
     var a = [-10, -10], b = [10, 10]
@@ -26,5 +27,28 @@ new Suite()
   })
   .on('complete', function() {
     console.log('- Fastest is ' + this.filter('fastest').map('name') + '\n');
+    runNextSuite()
   })
   .run({ 'async': true });
+
+
+function runNextSuite () {
+  console.log("MULTIPLE SEGMENT MULTIPLE OUTPUT TEST")
+  new Suite('Multiple Segments - Multiple Outputs')
+  .add('kwc', () => {
+    kwc([[-5, 10], [0, -10], [5, 10]], bbox);
+  })
+  .add('mapbox/lineclip', () => {
+    lineclip([[-5, 10], [0, -10], [5, 10]], bbox);
+  })
+  .on('cycle', function(event) {
+    console.log(event.target.toString());
+  })
+  .on('error', function(e) {
+    throw e.target.error;
+  })
+  .on('complete', function() {
+    console.log('- Fastest is ' + this.filter('fastest').map('name') + '\n');
+  })
+  .run({ 'async': true });
+}
